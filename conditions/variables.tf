7a3665af6637cd1dacc9f8fbfb032cd0126ce738
Raghu <raghu.mobility@gmail.com>
2025-01-29 17:22:04 +0530

    terraform

---
+++ conditions/variables.tf
@@ -0,0 +1,45 @@
+variable "ami_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "This is RHEL9 AMI Id"
+  
+}
+variable "environment" {
+  default = "prod"
+}
+variable "instance_type" {
+    default = "t3.micro"
+  
+}
+variable "ec2_tags" {
+    type = map
+    default = {
+        project = "expense"
+        Component = "backend"
+        Environment ="dev"
+        Name = "expense-backend-dev"
+    }
+  
+}
+
+variable "from_port" {
+  type = number
+  default = 22
+}
+variable "to_port" {
+    type = number
+    default = 22
+  
+}
+variable "cdir_blocks" {
+    type = list
+    default = ["0.0.0.0/0"]
+  
+}
+variable "sg_tags" {
+    type = map
+    default = {
+    Name ="expense-backend-dev"
+
+    }  
+}
